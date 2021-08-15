7a455cfba3199667c55674e0a7deeb74a4116b12
Eduardo Moreira <contato@eduardoem.com.br>
2021-08-15 08:30:16 -0300

    Add custom styles to storybook

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,5 +2,10 @@ import { addons } from '@storybook/addons';
 import { themes } from '@storybook/theming';
 
 addons.setConfig({
-  theme: themes.dark,
+  theme: {
+    ...themes.dark,
+    brandImage: 'https://user-images.githubusercontent.com/321738/63501763-88dbf600-c4cc-11e9-96cd-94adadc2fd72.png',
+    brandTitle: 'CIG Platform - UI',
+    brandUrl: './',
+  },
 });
