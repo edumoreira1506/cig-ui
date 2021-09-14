a7b02746e9761cafd96bfc7c5ded42e42ad2c16c
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-14 20:40:46 -0300

    Add default border radius valus constant

--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -1,8 +1,11 @@
 export enum Colors {
   DarkGrey = '#484848',
-  LightGrey = '#d7d7d7'
+  LightGrey = '#d7d7d7',
+  DarkBlue = '#2a3c5a'
 }
 
 export const MAIN_FONT = '\'Raleway\', sans-serif';
 
 export const DEFAULT_TRANSITION = 'ease 0.2s';
+
+export const DEFAULT_BORDER_RADIUS = '5px';
