4ddc978655e3717fbedcc628a9c70b19318cc276
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-14 00:09:11 -0300

    Add tests

--- src/organisms/Input/Input.tsx
+++ src/organisms/Input/Input.tsx
@@ -5,19 +5,20 @@ import { StyledContainer, StyledInput, StyledLabel } from './Input.styles';
 export interface InputProps {
   value: string | number;
   onChange: (value: string | number) => void;
-  type: 'text' | 'number' | 'email';
+  type?: 'text' | 'number' | 'email';
   label?: string;
   name?: string;
   placeholder?: string;
+  inputTestId?: string;
 }
 
-export default function Input({ value, onChange, type, label, name, placeholder }: InputProps) {
+export default function Input({ value, onChange, type = 'text', label, name, placeholder, inputTestId }: InputProps) {
   const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => onChange(e.target.value), [onChange]);
 
   return (
     <StyledContainer>
       {label && <StyledLabel htmlFor={name}>{label}</StyledLabel>}
-      <StyledInput placeholder={placeholder} id={name} name={name} value={value} onChange={handleChange} type={type} />
+      <StyledInput data-testid={inputTestId} placeholder={placeholder} id={name} name={name} value={value} onChange={handleChange} type={type} />
     </StyledContainer>
   );
 }
