5abe036a079c61ce8b1b93e51e17db0443e02aa6
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-15 19:18:51 -0300

    Add Select component

---
+++ src/atoms/Select/Select.styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+import { Colors, DEFAULT_BORDER_RADIUS, MAIN_FONT } from '../../constants/styles';
+
+export const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  font-family: ${MAIN_FONT};
+`;
+
+export const StyledSelect = styled.select`
+  background-color: ${Colors.LightGrey};
+  border: none;
+  width: 100%;
+  padding: 10px 0 10px 10px;
+  border-radius: ${DEFAULT_BORDER_RADIUS};
+  color: ${Colors.DarkGrey};
+
+  &:focus {
+    outline: none;
+  }
+`;
