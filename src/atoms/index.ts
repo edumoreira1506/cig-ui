e47114922b3fb63bf2de9aaa1d0cf7cb4ee14e73
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-15 19:27:22 -0300

    Bump to 0.1.12

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -5,3 +5,5 @@ export { default as Title } from './Title/Title';
 export { default as Label } from './Label/Label';
 export { default as FormField } from './FormField/FormField';
 export { default as Button } from './Button/Button';
+export { default as Subtitle } from './Subtitle/Subtitle';
+export { default as Select } from './Select/Select';
