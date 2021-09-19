b655dcbc47e4ea0c563294d8a4bbe6c45c1d094f
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-19 08:45:36 -0300

    Move Select and TextField to molecules folder

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -2,8 +2,6 @@ export { default as Button } from './Button/Button';
 export { default as FormField } from './FormField/FormField';
 export { default as Label } from './Label/Label';
 export { default as Loading } from './Loading/Loading';
-export { default as Select } from './Select/Select';
 export { default as Subtitle } from './Subtitle/Subtitle';
 export { default as Tabs } from './Tabs/Tabs';
-export { default as TextField } from './TextField/TextField';
 export { default as Title } from './Title/Title';
--- src/molecules/index.ts
+++ src/molecules/index.ts
@@ -1,1 +1,3 @@
 export { default as Input } from './Input/Input';
+export { default as Select } from './Select/Select';
+export { default as TextField } from './TextField/TextField';
