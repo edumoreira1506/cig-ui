fd42b5a559b1f81b2db06dd36a07cef1630ae36d
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-19 09:15:27 -0300

    Export RoundImage

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -5,3 +5,4 @@ export { default as Loading } from './Loading/Loading';
 export { default as Subtitle } from './Subtitle/Subtitle';
 export { default as Tabs } from './Tabs/Tabs';
 export { default as Title } from './Title/Title';
+export { default as RoundImage } from './RoundImage/RoundImage';
