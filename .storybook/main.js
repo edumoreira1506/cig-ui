a7307267ec7779c3b55145a1010848a78a6b0ca7
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-07 23:15:21 -0300

    Fix storybook build

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,21 @@
+const path = require('path')
+
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  webpackFinal: (config) => {
+    config.resolve.alias['swiper/react'] = path.resolve(__dirname, '../', 'node_modules', 'swiper', 'react', 'swiper-react');
+    config.resolve.alias['swiper'] = path.resolve(__dirname, '../', 'node_modules', 'swiper', 'swiper.esm');
+    config.resolve.alias['ssr-window'] = path.resolve(__dirname, '../', 'node_modules', 'ssr-window', 'ssr-window.esm');
+    config.resolve.alias['dom7'] = path.resolve(__dirname, '../', 'node_modules', 'dom7', 'dom7.esm');
+
+    return config;
+  },
 }
