550679e2553ed08ed762e448c30c7019e8811a53
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-08 00:17:43 -0300

    Add slick carousel

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,11 +11,6 @@ module.exports = {
     "@storybook/preset-create-react-app"
   ],
   webpackFinal: (config) => {
-    config.resolve.alias['swiper/react'] = path.resolve(__dirname, '../', 'node_modules', 'swiper', 'react', 'swiper-react');
-    config.resolve.alias['swiper'] = path.resolve(__dirname, '../', 'node_modules', 'swiper', 'swiper.esm');
-    config.resolve.alias['ssr-window'] = path.resolve(__dirname, '../', 'node_modules', 'ssr-window', 'ssr-window.esm');
-    config.resolve.alias['dom7'] = path.resolve(__dirname, '../', 'node_modules', 'dom7', 'dom7.esm');
-
     return config;
   },
 }
