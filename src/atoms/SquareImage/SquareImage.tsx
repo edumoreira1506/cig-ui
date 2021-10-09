8e5c741dda0e3b10bed337fa0b2aa251951f7a09
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-09 16:19:04 -0300

    Bump to 0.1.43

--- src/atoms/SquareImage/SquareImage.tsx
+++ src/atoms/SquareImage/SquareImage.tsx
@@ -1,14 +1,17 @@
+import { useCallback } from 'react';
 import { StyledContainer, StyledImage } from './SquareImage.styles';
 
 export interface SquareImageProps {
   alt?: string;
   src: string;
-  onClick?: () => void;
+  onClick: (src: string) => void;
 }
 
 export default function SquareImage({ src, alt = '', onClick = () => undefined }: SquareImageProps) {
+  const handleClick = useCallback(() => onClick(src), [src, onClick]);
+
   return (
-    <StyledContainer onClick={onClick}>
+    <StyledContainer onClick={handleClick}>
       <StyledImage src={src} alt={alt} />
     </StyledContainer>
   );
