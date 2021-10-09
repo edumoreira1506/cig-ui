37460f6424c2fa62e0c4552c2940a73b600867db
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-09 15:46:55 -0300

    Bump to 0.1.42

--- src/molecules/index.ts
+++ src/molecules/index.ts
@@ -1,4 +1,6 @@
+export { default as FileImagesCarousel } from './FileImagesCarousel/FileImagesCarousel';
+export { default as Header } from './Header/Header';
 export { default as Input } from './Input/Input';
 export { default as Select } from './Select/Select';
+export { default as Sidebar } from './Sidebar/Sidebar';
 export { default as TextField } from './TextField/TextField';
-export { default as Header } from './Header/Header';
