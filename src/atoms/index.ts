f32acce7c69aec178ac809412f5ad6cf4f913892
Eduardo Moreira <contato@eduardoem.com.br>
2021-11-02 14:07:29 -0300

    Export Modal

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -6,6 +6,7 @@ export { default as ImageGallery } from './ImageGallery/ImageGallery';
 export { default as Label } from './Label/Label';
 export { default as LinkButton } from './LinkButton/LinkButton';
 export { default as Loading } from './Loading/Loading';
+export { default as Modal } from './Modal/Modal';
 export { default as RoundFileInput } from './RoundFileInput/RoundFileInput';
 export { default as RoundImage } from './RoundImage/RoundImage';
 export { default as SandwichButton } from './SandwichButton/SandwichButton';
