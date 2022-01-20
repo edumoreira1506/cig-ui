35f73a0ebc356360b04c96937566fdd7987831f2
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-19 21:35:47 -0300

    Update timeline styles (#35)
    * Change colors of timeline components
    
    * Bump to 0.1.73

--- src/atoms/Card/Card.styles.ts
+++ src/atoms/Card/Card.styles.ts
@@ -5,17 +5,17 @@ import { DEFAULT_BORDER_RADIUS, Colors, MAIN_FONT } from '../../constants';
 export const StyledContainer = styled.div`
   width: 100%;
   border-radius: ${DEFAULT_BORDER_RADIUS};
-  border: solid 1px ${Colors.LightGrey};
+  border: solid 1px ${Colors.DarkBlue};
   padding: 10px;
   box-sizing: border-box;
   font-family: ${MAIN_FONT};
-  background-color: ${Colors.White};
+  background-color: ${Colors.DarkBlue};
 `;
 
 export const StyledHeader = styled.div`
   display: flex;
   margin-bottom: 12px;
-  color: ${Colors.DarkGrey};
+  color: ${Colors.LightGrey};
   font-weight: bold;
 
   svg {
@@ -26,7 +26,7 @@ export const StyledHeader = styled.div`
 export const StyledBody = styled.div`
   font-weight: 300;
   margin-bottom: 20px;
-  color: ${Colors.Black};
+  color: ${Colors.White};
 `;
 
 export const StyledCTA = styled.button`
@@ -36,5 +36,6 @@ export const StyledCTA = styled.button`
   font-weight: bold;
   cursor: pointer;
   font-size: 12px;
+  color: ${Colors.White};
   padding: 0;
 `;
