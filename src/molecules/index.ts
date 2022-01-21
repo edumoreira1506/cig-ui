79cc343a9044a206911dfb694c9246e1e2be5f17
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-21 13:03:30 -0300

    Export Linksbar

--- src/molecules/index.ts
+++ src/molecules/index.ts
@@ -2,6 +2,7 @@ export { default as Autocomplete } from './Autocomplete/Autocomplete';
 export { default as FileImagesCarousel } from './FileImagesCarousel/FileImagesCarousel';
 export { default as Header } from './Header/Header';
 export { default as Input } from './Input/Input';
+export { default as LinksBar } from './LinksBar/LinksBar';
 export { default as PoultriesCarousel } from './PoultriesCarousel/PoultriesCarousel';
 export { default as Select } from './Select/Select';
 export { default as Sidebar } from './Sidebar/Sidebar';
