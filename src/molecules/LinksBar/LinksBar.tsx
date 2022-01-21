ab296ba3416ac5c429c9ce0a8100f9687e6983af
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-21 16:53:33 -0300

    Bump to 0.1.79

--- src/molecules/LinksBar/LinksBar.tsx
+++ src/molecules/LinksBar/LinksBar.tsx
@@ -10,7 +10,7 @@ interface LinksBarProps {
 const LinksBar: VFC<LinksBarProps> = ({ items }: LinksBarProps) => (
   <StyledContainer>
     {items.map((item) => (
-      <SquareLink key={item.href} href={item.href}>
+      <SquareLink key={item.href} href={item.href} onClick={item.onClick}>
         {item.children}
       </SquareLink>
     ))}
