cbecaf181a0c0b9bde2299592ae8aaff0b55e3e2
Eduardo Moreira <contato@eduardoem.com.br>
2022-02-07 22:22:55 -0300

    Fix tests

--- src/atoms/List/List.test.tsx
+++ src/atoms/List/List.test.tsx
@@ -10,23 +10,45 @@ const DEFAULT_PROPS = {
 
 describe('<List />', () => {
   it('renders the items', () => {
-    const items = ['text', 'text 2', 'text 3'];
+    const items = [
+      {
+        content: 'text',
+        key: 'text',
+      },
+      {
+        content: 'text 2',
+        key: 'text 2'
+      },
+      {
+        content: 'text 3',
+        key: 'text 3'
+      }
+    ];
 
     render(<List {...DEFAULT_PROPS} items={items} />);
 
     items.forEach(item => {
-      expect(screen.getByText(item)).toBeInTheDocument();
+      expect(screen.getByText(item.content)).toBeInTheDocument();
     });
   });
 
   it('calls onItemClick', () => {
-    const items = ['item 1', 'item 2'];
+    const items = [
+      {
+        content: 'item 1',
+        key: 'item 1'
+      },
+      {
+        content: 'item 2',
+        key: 'item 2'
+      }
+    ];
     const onItemClick = jest.fn();
 
     render(<List {...DEFAULT_PROPS} items={items} onItemClick={onItemClick} />);
 
     items.forEach(item => {
-      userEvent.click(screen.getByText(item));
+      userEvent.click(screen.getByText(item.content));
     });
 
     expect(onItemClick).toHaveBeenCalledTimes(items.length);
