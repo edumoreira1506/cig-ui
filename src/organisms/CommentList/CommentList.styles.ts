04407fbbbf3da771c27e69485c8842452c9ddc1d
Eduardo Moreira <contato@eduardoem.com.br>
2022-02-16 01:01:44 -0300

    Chicken 460 (#57)
    * Add border
    
    * Bump to 0.1.112

--- src/organisms/CommentList/CommentList.styles.ts
+++ src/organisms/CommentList/CommentList.styles.ts
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
+import { Colors } from '../../constants';
+
 export const StyledComment = styled.div`
-  margin-bottom: 10px;
+  margin-bottom: 20px;
+  border-top: solid 1px ${Colors.LightGrey};
+  padding-top: 20px;
 `;
 
 export const StyledNewComment = styled.form``;
