712e569e9665c597bf7b4a5bd6347a597318d349
Eduardo Moreira <contato@eduardoem.com.br>
2022-02-25 23:19:51 -0300

    Add Expand prop to Expand comopnent

--- src/atoms/Expand/Expand.tsx
+++ src/atoms/Expand/Expand.tsx
@@ -1,4 +1,4 @@
-import React, { VFC, ReactNode, useState, useCallback } from 'react';
+import React, { VFC, ReactNode, useState, useCallback, useEffect } from 'react';
 import { AiOutlineDown } from 'react-icons/ai';
 
 import {
@@ -12,17 +12,25 @@ import {
 export type ExpandProps = {
   children: ReactNode | ReactNode[];
   title: string;
+  expand?: boolean;
 }
 
 export const Expand: VFC<ExpandProps> = ({
   children,
-  title
+  title,
+  expand
 }: ExpandProps) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   const handleToggleIsExpanded = useCallback(() =>
     setIsExpanded(prevIsExpanded => !prevIsExpanded)
   , []);
+
+  useEffect(() => {
+    if (typeof expand === 'boolean' && expand !== isExpanded) {
+      setIsExpanded(expand);
+    }
+  }, [expand, isExpanded]);
 
   return (
     <StyledContainer>
