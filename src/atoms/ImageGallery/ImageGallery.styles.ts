663d91ca0c8a3749a63bf936720414c8c6bb16bc
Eduardo Moreira <contato@eduardoem.com.br>
2022-03-09 16:16:46 -0300

    Chicken 679 (#76)
    * Add object-fit
    
    * Bump to 0.1.135

--- src/atoms/ImageGallery/ImageGallery.styles.ts
+++ src/atoms/ImageGallery/ImageGallery.styles.ts
@@ -2,4 +2,8 @@ import styled from 'styled-components';
 
 export const StyledContainer = styled.div`
   width: 100%;
+
+  .image-gallery-thumbnails-container img {
+    object-fit: contain;
+  }
 `;
