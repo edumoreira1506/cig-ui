3f8f88b3b9e7b8e7d7f1c8d59ea65c79576b3f33
edumoreira1506 <contato@eduardoem.com.br>
2022-05-26 22:49:58 -0300

    Improve SelectedList styles

--- src/molecules/SelectedList/SelectedList.styles.ts
+++ src/molecules/SelectedList/SelectedList.styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 import { Colors, MAIN_FONT } from '../../constants/styles';
 
@@ -19,22 +19,12 @@ export const StyledItem = styled.li`
   align-items: center;
   padding: 0 0 0 15px;
   border-bottom: solid 1px ${Colors.DarkGrey};
-  margin: 3px 0;
+  margin: 0;
   position: relative;
+  color: ${({ selected }: { selected: boolean }) => selected ? Colors.White : Colors.Black};
+  background-color: ${({ selected }: { selected: boolean }) => selected ? Colors.DarkGrey : Colors.White};
 
   &:first-child {
     border-top: solid 1px ${Colors.DarkGrey};
   }
-
-  ${({ selected }: { selected: boolean }) => selected && css`
-    &::before {
-      content: "";
-      position: absolute;
-      width: 6px;
-      height: calc(100% - 3px);
-      background-color: ${Colors.DarkGrey};
-      left: 0;
-      top: 0;
-    }
-  `}
 `;
